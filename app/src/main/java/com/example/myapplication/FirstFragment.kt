5613e1482ebfd3ae555d7e2a64a6c5ac4244983b
andreeaboth <both.andreea@gmail.com>
2023-04-06 12:20:48 +0300

    testing

--- app/src/main/java/com/example/myapplication/FirstFragment.kt
+++ app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -22,9 +22,10 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        //binging.root
         return binding.root
 
     }
